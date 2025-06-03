f42e0b04cedb07fb71f1a7665b3b59c398dfea33
Aaron Hidalgo <hidalgoaaron114@gmail.com>
2025-06-03 13:23:03 +0200

    Commit para ver en github

--- src/GitModeloA.java
+++ src/GitModeloA.java
@@ -4,7 +4,9 @@ public class GitModeloA {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola a tod@s");
-		System.ou.println("Hola de nuevo");
+		System.out.println("Hola de nuevo");
+		System.out.println("Hola ultimo cambio");
+
 
 	}
 
